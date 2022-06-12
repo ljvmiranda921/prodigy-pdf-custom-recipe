1eb395ba4a6ef904f6f2ec70bf756ed9e8f542b0
Lj Miranda <ljvmiranda@gmail.com>
2022-06-12 15:31:29 +0800

    Improve how inference utilities work

--- scripts/recipes/qa.py
+++ scripts/recipes/qa.py
@@ -5,19 +5,20 @@ from typing import Iterable, List, Optional, Union, Dict
 
 from wasabi import msg
 from scripts.infer_utils import infer
+from scripts.constants import CLASS_NAMES
 
 try:
     import prodigy
     from prodigy.components.loaders import Images
     from prodigy.types import StreamType
-    from prodigy.util import split_string
+    from prodigy.util import split_string, get_labels
 except ImportError:
     msg.fail("No installation of prodigy found", exits=1)
 
 
 def make_labels(model_path: str, stream: StreamType) -> StreamType:
     """Add the predicted labels in the 'labels' key of the image spans"""
-    predictions = infer(model_path, list(stream))
+    predictions = infer(model_path, examples=list(stream), labels=CLASS_NAMES)
 
     for eg, pred in zip(stream, predictions):
         task = copy.deepcopy(eg)
@@ -49,7 +50,7 @@ def make_bboxes(bbox_path: str, stream: StreamType) -> StreamType:
     source=("Data to qa/annotate (directory of images, file path or '-' to read from standard input)", "positional", None, str),
     bbox_path=("Path to the bounding box annotations file (this model doesn't have OCR installed)", "positional", None, str),
     model_path=("Path to the finetuned model for inference", "positional", None, str),
-    label=("Comma-separated label(s) to annotate or text file with one label per line", "option", "l"),
+    label=("Comma-separated label(s) to annotate or text file with one label per line", "option", "l", get_labels),
     exclude=("Comma-separated list of dataset IDs whose annotations to exclude", "option", "e", split_string),
     darken=("Darken image to make boxes stand out more", "flag", "D", bool),
     # fmt: on
