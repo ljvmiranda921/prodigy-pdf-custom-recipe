ad16e0cc342a22d5b10ecb93f48351cc29d6e009
Lj Miranda <ljvmiranda@gmail.com>
2022-06-12 13:01:28 +0800

    Make base model and class names constant

--- scripts/infer_utils.py
+++ scripts/infer_utils.py
@@ -1,9 +1,16 @@
 from transformers import LayoutLMv3ForTokenClassification, LayoutLMv3Processor
 
+from scripts.constants import BASE_MODEL
+
 
 def load_model(model_path: str):
     model = LayoutLMv3ForTokenClassification.from_pretrained(model_path)
     return model
+
+
+def load_processor(model: str = BASE_MODEL):
+    processor = LayoutLMv3Processor.from_pretrained(model)
+    return processor
 
 
 def infer(example, model):
