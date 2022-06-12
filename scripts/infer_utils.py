3692dc12be674af724d4224ec74a7f73bed77cf2
Lj Miranda <ljvmiranda@gmail.com>
2022-06-12 15:42:43 +0800

    Update inference utilities with HF loaders

--- scripts/infer_utils.py
+++ scripts/infer_utils.py
@@ -2,6 +2,7 @@ from typing import List, Dict
 from transformers import LayoutLMv3ForTokenClassification, LayoutLMv3Processor
 from PIL import Image
 from wasabi import msg
+import numpy as np
 
 from scripts.constants import BASE_MODEL
 
@@ -18,10 +19,13 @@ def _load_processor(model: str = BASE_MODEL):
     return processor
 
 
-# def _process_image(image, model, processor, labels, id2label):
-#     width, height = image.size
-
-#     # Encode the image
+def _unnormalize_box(bbox, width, height):
+    return [
+        width * (bbox[0] / 1000),
+        height * (bbox[1] / 1000),
+        width * (bbox[2] / 1000),
+        height * (bbox[3] / 1000),
+    ]
 
 
 def infer(model_path: str, examples: List[Dict], labels: List[str]) -> List[str]:
@@ -29,6 +33,8 @@ def infer(model_path: str, examples: List[Dict], labels: List[str]) -> List[str]
     processor = _load_processor()
     id2label = {v: k for v, k in enumerate(labels)}
 
+    all_preds = []
+    all_bboxes = []
     for eg in examples:
         image = Image.open(eg["path"]).convert("RGB")
         width, height = image.size
@@ -50,4 +56,21 @@ def infer(model_path: str, examples: List[Dict], labels: List[str]) -> List[str]
         # Get the predictions
         predictions = outputs.logits.argmax(-1).squeeze().tolist()
         token_boxes = encoding.bbox.squeeze().tolist()
-        breakpoint()
+
+        # Only keep non-subword predictions
+        is_subword = np.array(offset_mapping.squeeze().tolist())[:, 0] != 0
+        true_predictions = [
+            id2label[pred]
+            for idx, pred in enumerate(predictions)
+            if not is_subword[idx]
+        ]
+        true_boxes = [
+            _unnormalize_box(box, width, height)
+            for idx, box in enumerate(token_boxes)
+            if not is_subword[idx]
+        ]
+
+        all_preds.append(true_predictions)
+        all_bboxes.append(true_boxes)
+
+    breakpoint()
